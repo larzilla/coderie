7ba93fbbf22d0e2f6f17f1c57bdb6a5963469f68
Larz <larsmith11@gmail.com>
2017-03-06 15:21:31 -0500

    Adding decoding feature

--- scripts/coderie.js
+++ scripts/coderie.js
@@ -7,26 +7,35 @@ var t = document.getElementById.bind(document),
     output = t('output'),
     switchBox = t('switchbox'),
     badChars = /[-~<>&#%\r\n\t]/g;
-    
 function encodeIt(e) {  
   var entry = raw.value;
   var encodedSvg = entry.replace(/\"/g, "'");
-      encodedSvg = encodedSvg.replace(badChars, escape);
+      encodedSvg = encodedSvg.replace(badChars, encodeURIComponent);
   var cssBg = 'background-image: url("data:image/svg+xml,' + encodedSvg + '");';
   
   //Output
   encoded.value = encodedSvg;
+  compileIt(cssBg);
+  e.preventDefault();
+}
+function decodeIt(e) {  
+  var entry = encoded.value;
+  var decodedSvg = decodeURIComponent(entry.replace(/\'/g,  '"'));
+  var cssBg = 'background-image: url("data:image/svg+xml,' + entry + '");';
+  
+  //Output
+  raw.value = decodedSvg;
+  compileIt(cssBg);
+  e.preventDefault();
+}
+function compileIt(cssBg) {
   compiled.value = cssBg;
   output.setAttribute("style", cssBg);
 }
-
-document.getElementById('editableDiv').addEventListener('paste', handlePaste);
 
 //Scheme color
 function changeScheme() {
  output.parentElement.classList.toggle("dark");
 }
-
-//Copy to clipboard
 
 
